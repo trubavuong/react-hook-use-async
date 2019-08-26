b260dd7da23b709cbf74da14a9fb71920846c887
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-26 16:18:19 +0700

    Add Typescript declaration file.

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+interface AsyncResult<Result> {
+  error?: Error,
+  result?: Result,
+  promise?: Promise<Result>,
+  isPending: boolean,
+  cancel: () => void,
+  execute: () => void,
+}
+
+interface Config<Result, Inputs> {
+  onError?: (error: Error, inputs: Inputs) => void,
+  onSuccess?: (result: Result, inputs: Inputs) => void,
+}
+
+declare function useAsync<Result, Inputs extends any[]>(
+  createTask: (inputs: Inputs) => Promise<Result> | Result,
+  inputs?: Inputs,
+  config?: Config<Result, Inputs>,
+): AsyncResult<Result>;
+
+declare function useAsyncOnDemand<Result, Inputs extends any[]>(
+  createTask: (inputs: Inputs) => Promise<Result> | Result,
+  inputs?: Inputs,
+  config?: Config<Result, Inputs>,
+): AsyncResult<Result>;
+
+export default useAsync;
+export { useAsyncOnDemand };
