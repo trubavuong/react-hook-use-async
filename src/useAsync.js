47f832d724c661f26be461bed5853e24b29f287e
StevenTea <5902761+StevenTea@users.noreply.github.com>
2019-08-29 11:24:41 +0700

    Simplify code.

--- src/useAsync.js
+++ src/useAsync.js
@@ -1,38 +1,6 @@
-import {
-  useRef,
-  useState,
-  useEffect,
-  useReducer,
-} from 'react';
+import { useRef, useState, useEffect } from 'react';
 
-const noop = () => {};
-
-const Action = {
-  EXECUTE: 'execute',
-  RESOLVE: 'resolve',
-  REJECT: 'reject',
-};
-
-function taskReducer(state, { type, payload }) {
-  switch (type) {
-    case Action.EXECUTE:
-      return { ...state, isPending: true };
-
-    case Action.RESOLVE:
-      return { result: payload, isPending: false };
-
-    case Action.REJECT:
-      return { error: payload, isPending: false };
-
-    /* istanbul ignore next */
-    default:
-      return state;
-  }
-}
-
-function initTaskState() {
-  return { isPending: false };
-}
+const noop = () => { };
 
 function createAbortError() {
   const error = new Error('The operation was aborted');
@@ -47,20 +15,21 @@ function executeTask(createTask, inputs) {
 
   const injection = { abortSignal };
 
-  let isCancelled = false;
+  let isCanceled = false;
   const cancel = () => {
-    isCancelled = true;
+    isCanceled = true;
     abortByController();
   };
 
   let proxyPromise;
   try {
+    const abortError = createAbortError();
     const task = createTask(inputs, injection);
     const taskPromise = (task instanceof Promise ? task : Promise.resolve(task));
     proxyPromise = new Promise((resolve, reject) => {
       taskPromise.then(
-        result => (isCancelled ? reject(createAbortError()) : resolve(result)),
-        error => (isCancelled ? reject(createAbortError()) : reject(error)),
+        result => (isCanceled ? reject(abortError) : resolve(result)),
+        error => (isCanceled ? reject(abortError) : reject(error)),
       );
     });
   }
@@ -69,24 +38,6 @@ function executeTask(createTask, inputs) {
   }
 
   return { cancel, promise: proxyPromise };
-}
-
-function executeTaskOnEffect(dispatch, createTask, inputs, { onError = noop, onSuccess = noop }) {
-  const task = executeTask(createTask, inputs);
-  dispatch({ type: Action.EXECUTE });
-
-  task.promise.then(
-    result => {
-      dispatch({ type: Action.RESOLVE, payload: result });
-      onSuccess(result, inputs);
-    },
-    error => {
-      dispatch({ type: Action.REJECT, payload: error });
-      onError(error, inputs);
-    },
-  );
-
-  return task;
 }
 
 function useAsyncInternal(createTask, inputs, { onError, onSuccess, isOnDemand }) {
@@ -105,18 +56,31 @@ function useAsyncInternal(createTask, inputs, { onError, onSuccess, isOnDemand }
     setCount(prevCount => prevCount + 1);
   };
 
-  const [state, dispatch] = useReducer(taskReducer, {}, initTaskState);
+  const [state, setState] = useState({ isPending: false });
   useEffect(
     () => {
       if (staticConfig.isOnDemand && count === 0) {
         return noop;
       }
 
-      const task = executeTaskOnEffect(
-        dispatch,
-        self.createTask,
-        inputs,
-        { onError: self.onError, onSuccess: self.onSuccess },
+      const {
+        createTask: createTaskSelf,
+        onError: onErrorSelf = noop,
+        onSuccess: onSuccessSelf = noop,
+      } = self;
+
+      const task = executeTask(createTaskSelf, inputs);
+      setState(prevState => ({ ...prevState, isPending: true }));
+
+      task.promise.then(
+        result => {
+          setState({ result, isPending: false });
+          onSuccessSelf(result, inputs);
+        },
+        error => {
+          setState({ error, isPending: false });
+          onErrorSelf(error, inputs);
+        },
       );
       self.task = task;
 
